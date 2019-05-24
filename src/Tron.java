0f2fb6198d8e9134594e95b1b1c647807ddd8e85
mKir04 <strongsnow@gmail.com>
2019-05-24 11:06:24 -0400

    hdowa

--- src/Tron.java
+++ src/Tron.java
@@ -1,2 +1,14 @@
-public class Tron {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class Tron extends Application {
+    public void start(Stage ps) {
+        Pane pane = new Pane();
+
+        Scene scene = new Scene(pane);
+        ps.setScene(scene);
+        ps.show();
+    }
 }
