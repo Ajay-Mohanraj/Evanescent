5dd2c00f4a0b7dddbce19ca0e5c35c845b355d3b
Eric Kim <erikim22@bergen.org>
2019-05-29 13:23:41 -0400

    g

--- src/Tron.java
+++ src/Tron.java
@@ -7,24 +7,24 @@ import javafx.stage.Stage;
 
 public class Tron extends Application {
     Pane pane = new Pane();
-    Image player1 = new Image("YellowRight.gif", 150, 150, true, true);
-    ImageView p1 = new ImageView(player1);
-    Image player2 = new Image("ThanosLeft.gif", 150, 150, true, true);
-    ImageView p2 = new ImageView(player2);
-    Image thanosRight  = new Image("ThanosRight.gif", 150, 150, true., true);
-    ImageView tR = new ImageView(thanosRight);
+
+    Image yellowRight = new Image("YellowRight.gif", 150, 150, true, true);
+    Image yellowLeft = new Image("YellowLeft.gif", 150, 150, true, true);
+    Image yellowUp = new Image("YellowUp.gif", 150, 150, true, true);
+    Image yellowDown = new Image("YellowDown.gif", 150, 150, true, true);
+
+    Image thanosLeft = new Image("ThanosLeft.gif", 150, 150, true, true);
+    Image thanosRight  = new Image("ThanosRight.gif", 150, 150, true, true);
     Image thanosUp = new Image("ThanosUp.gif", 150, 150, true, true);
-    ImageView tU = new ImageView(thanosUp);
     Image thanosDown = new Image("ThanosDown.gif", 150, 150, true, true);
-    ImageView tD = new ImageView(thanosDown);
+    ImageView p1 = new ImageView(yellowRight);
+    ImageView p2 = new ImageView(thanosLeft);
     public void start(Stage ps) {
         p1.setX(750);
         p1.setY(750);
 
         p2.setX(150);
         p2.setY(150);
-
-
 
         pane.getChildren().addAll(p1, p2);
 
