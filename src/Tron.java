f1c61691ac6ba769ecd5da668d188b4187d1c54e
mKir04 <strongsnow@gmail.com>
2019-05-29 12:39:53 -0400

    hdowa

--- src/Tron.java
+++ src/Tron.java
@@ -1,5 +1,7 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
@@ -7,8 +9,22 @@ public class Tron extends Application {
     public void start(Stage ps) {
         Pane pane = new Pane();
 
+        Image player1 = new Image("ThanosTron.png", 150, 150, true, true);
+        ImageView p1 = new ImageView(player1);
+        p1.setX(150);
+        p1.setY(150);
+
+
+        Image player2 = new Image("YellowCar.png", 150, 150, true, true);
+        ImageView p2 = new ImageView(player2);
+        p2.setX(750);
+        p2.setY(750);
+
+        pane.getChildren().addAll(p1, p2);
+
         Scene scene = new Scene(pane);
         ps.setScene(scene);
         ps.show();
     }
 }
+
