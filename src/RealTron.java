cb188bc6356d6722f6ca96801ab73a5ec1bb9642
mKir04 <strongsnow@gmail.com>
2019-06-11 12:39:40 -0400

    drawPath

--- src/RealTron.java
+++ src/RealTron.java
@@ -2,15 +2,22 @@ import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.event.Event;
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
+
+import java.util.ArrayList;
 
 public class RealTron extends Application {
     Pane pane = new Pane();
@@ -19,8 +26,13 @@ public class RealTron extends Application {
     Image green= new Image("Green.png", 96, 96, true, true);
     Image thanos = new Image("Thanos.png", 96, 96, true, true);
 
-    ImageView p1 = new ImageView(green);
-    ImageView p2 = new ImageView(thanos);
+    ImageView p1 = new ImageView(thanos);
+    ImageView p2 = new ImageView(green);
+
+    BorderPane end = new BorderPane();
+    Text winner = new Text("");
+
+    ArrayList<Rectangle> paths = new ArrayList<Rectangle>();
 
     int p1Dx = 0;
     int p1Dy = 0;
@@ -115,15 +127,33 @@ public class RealTron extends Application {
                 p1.setY(p1.getY()+p1Dy);
                 p2.setX(p2.getX()+p2Dx);
                 p2.setY(p2.getY()+p2Dy);
+                drawPath(p1, Color.INDIGO);
+                drawPath(p2, Color.LIGHTGREEN);
 
                 if (hits(p1, p2)) {
-                    Pane end = new Pane();
-                    Text winner = new Text("YOU BOTH LOSE!");
-
+                    winner.setTextAlignment(TextAlignment.CENTER);
+                    if (p1.getRotate() == 0 && p2.getRotate() == 180 ||
+                        p1.getRotate() == 180 && p2.getRotate() == 0 ||
+                        p1.getRotate() == 90 && p2.getRotate() == 270 ||
+                        p1.getRotate() == 270 && p2.getRotate() == 90) {
+                        winner.setText("YOU BOTH LOSE!");
+                    }
+                    else if (p1.getRotate() == 0 && p2.getRotate() == 90 && p1.getX() + 9 < p2.getX() + 27 && p2.getX() + 66 < p1.getX() + 84 ||
+                            p1.getRotate() == 0 && p2.getRotate() == 270 && p1.getX() + 9 < p2.getX() + 27 && p2.getX() + 66 < p1.getX() + 84 ||
+                            p1.getRotate() == 180 && p2.getRotate() == 90 && p1.getX() + 9 < p2.getX() + 27 && p2.getX() + 66 < p1.getX() + 84 ||
+                            p1.getRotate() == 180 && p2.getRotate() == 270 && p1.getX() + 9 < p2.getX() + 27 && p2.getX() + 66 < p1.getX() + 84) {
+                        winner.setText("ThanosTron WINS!!!\nHALF YOUR RAM IS GONE!!!\nCLICK HERE TO DOWNLOAD MORE RAM");
+                    }
+                    else if (p2.getRotate() == 0 && p1.getRotate() == 90 && p2.getX() + 9< p1.getX() + 27 && p1.getX() + 66 < p2.getX() + 84 ||
+                            p2.getRotate() == 0 && p1.getRotate() == 270 && p2.getX() + 9 < p1.getX() + 27 && p1.getX() + 66 < p2.getX() + 84 ||
+                            p2.getRotate() == 180 && p1.getRotate() == 90 && p2.getX() + 9 < p1.getX() + 27 && p1.getX() + 66 < p2.getX() + 84 ||
+                            p2.getRotate() == 180 && p1.getRotate() == 270 && p2.getX() + 9 < p1.getX() + 27 && p1.getX() + 66 < p2.getX() + 84) {
+                        winner.setText("YOU HAVE SUCCESSFULLY SAVED YOUR RAM");
+                    }
                     winner.xProperty().bind(end.widthProperty().divide(2));
                     winner.yProperty().bind(end.heightProperty().divide(2));
 
-                    end.getChildren().add(winner);
+                    end.setCenter(winner);
 
                     Scene endGame = new Scene(end, 1000, 1000);
                     ps.setScene(endGame);
@@ -135,7 +165,6 @@ public class RealTron extends Application {
         ps.setScene(scene);
         ps.show();
         pane.requestFocus();
-
     }
 
     public void move(KeyEvent e) {
@@ -159,8 +188,48 @@ public class RealTron extends Application {
         return new Rectangle2D(0, 0, 1, 1);
     }
 
-    public void drawPath() {
+    public void drawPath(ImageView player, Color color) {
+        Rectangle path = new Rectangle();
 
+        if (player.getRotate() == 0) {
+            path.setX(player.getX() + 6);
+            path.setY(player.getY() + 27);
+            path.setWidth(5);
+            path.setHeight(42);
+        }
+
+        else if (player.getRotate() == 180) {
+            path.setX(player.getX() + 87);
+            path.setY(player.getY() + 27);
+            path.setWidth(5);
+            path.setHeight(42);
+        }
+
+        else if (player.getRotate() == 90) {
+            path.setX(player.getX() + 27);
+            path.setY(player.getY() + 6);
+            path.setWidth(42);
+            path.setHeight(5);
+        }
+
+        else if (player.getRotate() == 270) {
+            path.setX(player.getX() + 27);
+            path.setY(player.getY() + 87);
+            path.setWidth(42);
+            path.setHeight(5);
+        }
+
+        paths.add(path);
+
+        for (Rectangle rect : paths) {
+            if (rect.intersects(player.getX() + 9, player.getY() + 30, player.getImage().getRequestedWidth() - 18, player.getImage().getRequestedHeight() - 51)) {
+                BorderPane pane = new BorderPane();
+                pane.setCenter(new Text("It Works"));
+            }
+        }
+        path.setFill(color);
+        path.setStroke(color);
+        pane.getChildren().add(path);
     }
 
     public boolean hits(ImageView p1, ImageView p2) {
